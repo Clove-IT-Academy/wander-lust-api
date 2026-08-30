4673568b9a7ecd3f0273c058c53b80b894237b16
Shashank Jha <shashankj677@gmail.com>
2026-08-30 14:14:04 +0545

    added entrypoint file

---
+++ index.js
@@ -0,0 +1,23 @@
+// const express = require("express"); // import express but old technique
+import express from "express"; // import express using ES6 module syntax
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// traditional function declaration
+// function helloWorld(req, res) {
+//   res.send("Hello, World!");
+// }
+
+// const helloWorldArrow = (req, res) => {
+//   res.send("Hello, World!");
+// };
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+}); // using arrow function as a callback
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
