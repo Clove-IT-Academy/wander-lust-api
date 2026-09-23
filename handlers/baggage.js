c9960a35b8160c3dd240bdf869d7e4f58d321722
jeetenmndl <jeetenmndl96@gmail.com>
2026-09-23 14:51:02 +0545

    add mail configuration, conflict error class, and baggage CRUD operations

--- handlers/baggage.js
+++ handlers/baggage.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { createBaggageValidator } from "../validators/baggage";
-import { create, index } from "../services/baggage";
+import { createBaggageValidator, updateBaggageValidator } from "../validators/baggage";
+import { create, findOne, index, remove, update } from "../services/baggage";
 
 const router = Router();
 
@@ -16,6 +16,33 @@ router.post("/", createBaggageValidator, async (req, res, next) => {
 router.get("/", async (req, res, next) => {
   try {
     const baggage = await index(req.user, req.params.tripId);
+    res.status(200).json(baggage);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const baggage = await findOne(req.params.id, req.user);
+    res.status(200).json(baggage);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", updateBaggageValidator, async (req, res, next) => {
+  try {
+    const baggage = await update(req.params.id, req.body, req.user);
+    res.status(200).json(baggage);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const baggage = await remove(req.params.id, req.user);
     res.status(200).json(baggage);
   } catch (error) {
     next(error);
